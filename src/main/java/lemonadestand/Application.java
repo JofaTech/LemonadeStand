8181e38eedc0cad94f54d029de85cea9ac2a7cc8
Brandon Lukevich <brandon.lukevich@tbs.toshiba.com>
2025-05-16 12:05:29 -0400

    Added Exception Handling for Application.java and ViewOrder.java

--- src/main/java/lemonadestand/Application.java
+++ src/main/java/lemonadestand/Application.java
@@ -12,27 +12,27 @@ import lemonadestand.model.Order;
 
 public class Application {
 
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Welcome to the LemonadeStand Order Application!");
 		System.out.println("To make an order we need you to provide your name and phone number.");
 		System.out.println("Let's get started with your name:");
-		
+
 		String name = scanner.nextLine();
-		
+
 		System.out.println("Hi " + name + ", nice to meet you. ");
 		System.out.println("Next we need your number so that we can call you when your order is ready:");
 
 		String phoneNumber = scanner.nextLine();
-		
-		System.out.println("Awesome! We captured your phone number as " + 
-				phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3")); // Uses regex to format
+
+		System.out.println("Awesome! We captured your phone number as "
+				+ phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3")); // Uses regex to format
 		System.out.println("Is that correct?");
-		
+
 		String validation = "";
-		
+
 		do {
 			if (validation.equals("N")) {
 				System.out.println("Please re-enter your phone number:");
@@ -40,19 +40,19 @@ public class Application {
 				System.out.println("Now we have: " + phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"));
 				System.out.println("Is that correct?");
 			}
-			
+
 			System.out.println("Please enter Y (for yes), or N (for no)");
 			validation = scanner.nextLine();
 		} while (!validation.equals("Y"));
-		
+
 		System.out.println("Great! Let's get to your order then...");
-		
+
 		Customer customer = new Customer(name, phoneNumber);
 		Order order = new Order(customer);
-		
+
 		System.out.println("How many lemonades would you like to order?");
 		int numberOfLemonades = scanner.nextInt();
-		
+
 		for (int i = 0; i < numberOfLemonades; i++) {
 			System.out.println("\nLet's get the details of Lemonade #" + (i + 1) + "...");
 			System.out.println("How much lemon juice do you want (in cups)?");
@@ -63,25 +63,42 @@ public class Application {
 			double sugar = scanner.nextDouble();
 			System.out.println("How many ice cubes do you want?");
 			int iceCubes = scanner.nextInt();
-			
-			order.addLemonade(new Lemonade(lemonJuice, water, sugar, iceCubes));		
+
+			order.addLemonade(new Lemonade(lemonJuice, water, sugar, iceCubes));
 		}
-		
-		// Save the order somewhere		
+
+		// Save the order somewhere
 		File file = new File("./orders");
-		
+
 		File[] files = file.listFiles();
+
+		FileOutputStream fileOutputStream = null;
+		ObjectOutputStream objectOutputStream = null;
 		
-		FileOutputStream fileOutputStream = new FileOutputStream(file + "/order" + (files.length + 1) + ".txt");
-		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-		
-		objectOutputStream.writeObject(order);
-		
-		
+		try {
+			fileOutputStream = new FileOutputStream(file + "/order" + (files.length + 1) + ".txt");
+			objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+			objectOutputStream.writeObject(order);
+		} catch (IOException e) {
+			System.out.println("Failed to create file. Please ensure orders directory exists.");
+		} finally {
+			try {
+				if (fileOutputStream != null) {
+					fileOutputStream.close();
+				}
+				if (objectOutputStream != null) {
+					objectOutputStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
 		System.out.println("Successfully placed order for " + name + ".");
 		System.out.println("Your order total is: $" + order.getTotal());
 		System.out.println("Please be ready to pay when you pick up your order. Thank you!!");
-		
+
 	}
 
 }
--- src/main/java/lemonadestand/ViewOrder.java
+++ src/main/java/lemonadestand/ViewOrder.java
@@ -2,34 +2,54 @@ package lemonadestand;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import lemonadestand.model.Order;
 
 public class ViewOrder {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		Scanner scanner = new Scanner(System.in);
-		
-		File file = new File("./orders");
-	
+	public static void main(String[] args) {
+
 		while (true) {
+			Scanner scanner = new Scanner(System.in);
+
+			File file = new File("./orders");
+
 			System.out.println("Which order would you like to view?");
-		
-			int orderNumber = scanner.nextInt();
-		
-			FileInputStream fileInputStream = new FileInputStream(file + "/order" + orderNumber + ".txt");
-			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-			
-			Order order = (Order) objectInputStream.readObject();
-			
-			System.out.println(order.getLemonades());
-			System.out.println("Total= $" + order.getTotal());
-			
+
+			int orderNumber = 0;
+
+			try {
+				orderNumber = scanner.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter a number less than 2 Billion");
+			}
+
+			// try catch loop for Exception handling (try with resources syntax to ensure
+			// proper closing)
+			try (FileInputStream fileInputStream = new FileInputStream(file + "/order" + orderNumber + ".txt");
+					ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);) {
+
+				// Cast readObject to Order type (from default Object type)
+				Order order = (Order) objectInputStream.readObject();
+
+				System.out.println(order.getLemonades());
+				System.out.println("Total= $" + order.getTotal());
+			} catch (FileNotFoundException e) {
+				System.out.println("Order number " + orderNumber + " does not exist.");
+			} catch (IOException e) {
+				System.out.println("Internal IOException.");
+			} catch (ClassNotFoundException e) {
+				System.out.println("Tried to read in an order that isn't formatted correctly.");
+			} catch (ClassCastException e) {
+				System.out.println("The file read does not contain an order.");
+			}
+
 		}
-		
-		
+
 	}
 }
